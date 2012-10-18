ca5b0e1f881ee5cd49aa82d3a297bfc05fcf3f59
TeXHeX <TeXHeX@localhost>
2012-10-18 20:15:16 +0200

    Commit for Halt.h and AvailableMemory.h

---
+++ ArduUtilityLibrary/AvailableMemory.h
@@ -0,0 +1,110 @@
+//AvailableMemory.h
+/*
+availableMemory() Version 1, part of the ArduUtilityLibrary (http://aul.codeplex.com/)
+This function returns the number of free memory (in bytes) 
+Copyright (C) 2011-2012 Michael 'TeX' Hex
+
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+!!! Revision history:
+
+Version 1: First release
+
+
+!!! Documentation
+
+The most recent document can be found on the project homepage
+http://aul.codeplex.com/wikipage?title=AvailableMemory.h
+
+This is a snapshot of this page:
+
+
+Executing {{availableMemory()}} returns the number of free memory (in bytes) 
+
+!! Example
+
+{code:c++}
+void setup() { 
+ Serial.begin(9600);   
+}
+
+void loop() {
+  int iFreeMem=availableMemory();
+  
+  Serial.print(F("Free memory (bytes): "));
+  Serial.print(iFreeMem);
+  Serial.println();
+    
+  delay(1500);
+}
+{code:c++}
+
+!! Usage
+
+Execute {{availableMemory()}} to get the number of free bytes in memory. 
+
+Please note that memory requirements can change very fast in any sketch and even by executing this function the free memory is altered. Use the information as _one_ indicator if your sketch goes haywire.
+
+I have created sketches that reported 1100 free bytes in the main loop() function just to crash a second later because a different routine needed more memory than expected.
+
+!! References 
+
+This function is based on get_free_memory() by "Paul" described in the following post:
+[url:http://forum.pololu.com/viewtopic.php?p=4218&sid=7f231afba6e616f87950ffa4689d94f0#p4218]
+
+An advanced version of this code is freeRam() found here:
+[url:http://www.arduino.cc/playground/Code/AvailableMemory]
+
+The original code is also used inside the SdFat Library (SdFatUtil.h - FreeRam()) and documented in "Jonathan Oxer, Hugh Blemings: Practical Arduino: Cool Projects for Open Source Hardware", so I’m sticking with the original version.
+
+The name "availableMemory" is taken from "flash_example.pde", an example for the Flash library created by Mikal Hart 
+[url:http://arduiniana.org/libraries/flash/]
+
+
+*/
+
+
+
+
+#ifndef _AVAILABLEMEMORY_H
+#define _AVAILABLEMEMORY_H
+
+#include "Arduino.h"
+
+
+//Returns the number of free memory (in bytes) that is currently available. 
+int availableMemory()
+{
+	extern int  __bss_end;
+	extern int *__brkval;
+
+	int free_memory;
+
+	if((int)__brkval == 0)
+		free_memory = ((int)&free_memory) - ((int)&__bss_end);
+	else
+		free_memory = ((int)&free_memory) - ((int)__brkval);
+
+	return free_memory;
+}
+
+
+
+
+
+
+#endif
--- ArduUtilityLibrary/Halt.h
+++ ArduUtilityLibrary/Halt.h
@@ -1,4 +1,90 @@
 // Halt.h
+/*
+halt() Version 2, part of the ArduUtilityLibrary (http://aul.codeplex.com/)
+Execute halt() to stop execution of your sketch permanently
+Copyright (C) 2011-2012 Michael 'TeX' Hex
+
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+!!! Revision history:
+
+Version 1: First release
+Version 2: Added while(true)
+
+
+!!! Documentation
+
+The most recent document can be found on the project homepage
+http://aul.codeplex.com/wikipage?title=Halt.h
+
+This is a snapshot of this page:
+
+
+Calling {{halt()}} will halt execution of your sketch permanently (in _halt.h_)
+
+!!! Example
+
+{code:c++}
+void setup() { 
+  Serial.begin(9600);   
+
+  //Wait a little bit
+  delay(2000);
+}
+
+void loop() {
+  //Start countdown  
+  Serial.println(F("Three..."));
+  delay(1000);  
+  Serial.println(F("Two..."));
+  delay(1000);
+  Serial.println(F("One..."));
+  delay(1000);
+
+  halt();   //No code is executed beyond this line
+   
+  Serial.println(F("You will not see this message..."));
+}
+{code:c++}
+
+!!! Usage
+
+Execute {{halt()}} to stop execution of your sketch permanently.
+
+Please note that this function does *NOT* turn off any digital outputs. They will stay in the current state as they are when halt() is executed.
+
+If you have any project that needs an "Emergency Off" function (because it includes moving parts that could harm somebody), this function is not what you are searching for. You better use an external watchdog that turns off the board completely. 
+
+!!! References 
+
+Peter Knight (Cathedrow): Narcoleptic, Sleep library for Arduino
+[url:https://code.google.com/p/narcoleptic/]
+
+Rocket Scream: Lightweight Low Power Arduino Library
+[url:http://www.rocketscream.com/blog/2011/07/04/lightweight-low-power-arduino-library/]
+
+Ruben Laguna: Arduino Sleep Mode - Waking Up When Receiving Data on the USART
+[url:http://rubenlaguna.com/wp/2008/10/15/arduino-sleep-mode-waking-up-when-receiving-data-on-the-usart/index.html/]
+
+Some more information about Arduino sleep can be found in this playground post:
+[url:http://www.arduino.cc/playground/Learning/ArduinoSleepCode]
+
+*/
+
 
 #ifndef _HALT_h
 #define _HALT_h
